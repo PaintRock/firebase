4b7c42b0f5145a76596945a7569a17cb494e8d90
Adel Knode <ann.knode@atlasschool.com>
2025-04-27 17:07:47 +0000

    start here

---
+++ firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBctCB4LKKqpqybNfcIzHNHvpbhh0_EyHQ",
+  authDomain: "tut-fire-a99e5.firebaseapp.com",
+  projectId: "tut-fire-a99e5",
+  storageBucket: "tut-fire-a99e5.firebasestorage.app",
+  messagingSenderId: "218598448004",
+  appId: "1:218598448004:web:023b1a1d486181078fdbd1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
